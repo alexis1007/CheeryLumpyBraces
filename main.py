d557aea7de03e8423c30dc9368a3881ee8ce7001
Alexis Montalvo <alexis.1007@hotmail.com>
2024-11-07 19:28:29 +0000

    Forwad
    Primera actividad

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-"""
-This is the Template Repl for Python with Turtle.
+from turtle import *
 
-Python with Turtle lets you make graphics easily in Python.
+forward(100)
 
-Check out the official docs here: https://docs.python.org/3/library/turtle.html
-"""
-
-import turtle
-
-# Fullscreen the canvas
-screen = turtle.Screen()
-screen.setup(1.0, 1.0)
-
-# Begin!
-t = turtle.Turtle()
-
-for c in ['red', 'green', 'blue', 'yellow']:
-  t.color(c)
-  t.forward(75)
-  t.left(90)
-
-screen.mainloop()
+done()
